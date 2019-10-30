db57b9b79daa0026b6ea7f32a37edb556014e230
abhisheksingh24 <abhisheksingh_24@rediffmail.com>
2019-10-30 21:08:29 +0530

    Update aux_array_kmp.cpp

--- aux_array_kmp.cpp
+++ aux_array_kmp.cpp
@@ -1,7 +1,7 @@
 vector<int> createAux(string p){
         vector<int> aux(p.length(), 0);
         for(int i = 1, m = 0; i < p.length(); ){
-            if(p[m] == '?' || p[i] == p[m]) aux[i++] = ++m;
+            if(p[i] == p[m]) aux[i++] = ++m;
             else if(m) m = aux[m - 1];
             else aux[i++] = 0;
         }
